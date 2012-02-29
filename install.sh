2c2c440435d61b14a5115ca0a731e2a9271cee12
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-02-29 14:19:27 +0100

    small fixes to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
 apt-get install -y ruby1.9.1 ruby1.9.1-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl
 which chef-solo || gem1.9.1 install chef
+mkdir -p /var/chef/cache
 chef-solo -c config/solo.rb -j node.json
