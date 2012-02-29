818067e015fa8e1a95f69a40e6f89899b78cd5b7
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-02-29 14:13:50 +0100

    all chef dependencies need to be installed

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-which ruby || apt-get -y install ruby1.9.1
+apt-get install -y ruby1.9.1 ruby1.9.1-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl
 which chef-solo || gem1.9.1 install chef
 chef-solo -c config/solo.rb -j node.json
