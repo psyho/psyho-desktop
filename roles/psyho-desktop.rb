5547829fbcd37a29ab4319998d6a91eab6359f00
Adam Pohorecki <adam@pohorecki.pl>
2013-01-14 11:25:34 +0100

    Update roles/psyho-desktop.rb
    removed a bunch of unnecessary stuff

--- roles/psyho-desktop.rb
+++ roles/psyho-desktop.rb
@@ -3,12 +3,8 @@ description 'Psyhos desktop config'
 run_list *%w{
   recipe[google-chrome]
   recipe[rvm::user]
-  recipe[dropbox]
   recipe[nodejs]
   recipe[nodejs::npm]
   recipe[virtualbox]
-  recipe[memcached]
-  recipe[couchdb]
-  recipe[mongodb]
   recipe[system-packages]
 }
